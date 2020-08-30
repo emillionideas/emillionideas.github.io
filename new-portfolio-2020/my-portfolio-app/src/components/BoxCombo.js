fa38ac4a39d7a67e2087f7135172d187f4738988
emily8385 <ptran8385@gmail.com>
2020-08-30 12:48:16 -0400

    Create Nav Bar and Placehoder grid

---
+++ new-portfolio-2020/my-portfolio-app/src/components/BoxCombo.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./element.css"
+import BoxElement from "./BoxElement"
+
+function BoxCombo() {
+    return (
+        <div className="box-combo">
+          <BoxElement />
+          <BoxElement />
+          <BoxElement />
+        </div>
+    )
+}
+       
+export default BoxCombo
---
+++ new-portfolio-2020/my-portfolio-app/src/components/BoxElement.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./element.css"
+
+function BoxElement() {
+    return (
+        <div className="box-element">
+         <p className="note">Placeholder</p>
+        </div>
+    )
+}
+       
+export default BoxElement
